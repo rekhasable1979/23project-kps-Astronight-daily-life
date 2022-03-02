a85f5a506c8c9e8b468d35f8ecfb7f8dfc80357d
Krishna Sable <89785804+rekhasable1979@users.noreply.github.com>
2022-03-02 18:30:09 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,80 +1,59 @@
-var bg,sleep, brush, gym, eat, bath, move;
-var astronaut;
 
-function preload(){
-  bg= loadImage("images/iss.png");
-  sleep = loadAnimation("images/sleep.png");
-  brush = loadAnimation("images/brush.png");
-  gym = loadAnimation("images/gym1.png","images/gym1.png","images/gym2.png","images/gym2.png");
-  eat = loadAnimation("images/eat1.png","images/eat1.png","images/eat1.png","images/eat2.png","images/eat2.png","images/eat2.png");
-  bath = loadAnimation("images/bath1.png","images/bath1.png","images/bath1.png","images/bath2.png","images/bath2.png","images/bath2.png");
- move = loadAnimation("images/move1.png","images/move1.png","images/move2.png","images/move2.png");
-}
+
+
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies = Matter.Bodies
+const Body = Matter.Body 
+
 
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(400,400);
+//create the engine
+  //Add world to the engine
+  engine = Engine.create()
+  world = engine.world
+
+
   
-  astronaut = createSprite(300,230);
-  astronaut.addAnimation("sleeping", sleep);
-  astronaut.scale = 0.1;
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+  
+   ground=Bodies.rectangle(100,400,400,20,ground_options) 
+   World.add(world,ground)
+    
+
+  ball = Bodies.circle(100,10,20,ball_options);
+  World.add(world,ball);
+  
+ 
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  
+rect(ground.position.x,ground.position.y,500,20)
+
+  ellipse(ball.position.x,ball.position.y,20);
+  //write a rectangle function to display ground.
+ 
+
+
+  
   
 }
 
-function draw() {
-  background(bg);
-  drawSprites();
-
-  textSize(20);
-  fill("white")
-  text("Instructions:",20, 35);
-  textSize(15);
-  text("Up Arrow = Brushing",20, 55);
-  text("Down Arrow = Gymming",20, 70);
-  text("Left Arrow = Eating",20, 85);
-  text("Right Arrow = Bathing",20, 100);
-  text("m key = Moving",20, 115);
-  
-  edges=createEdgeSprites();
-  astronaut.bounceOff(edges);
-  
-  if(keyDown("UP_ARROW")){
-    astronaut.addAnimation("brushing", brush);
-    astronaut.changeAnimation("brushing");
-    astronaut.y = 350;
-    astronaut.velocityX = 0;
-    astronaut.velocityY = 0;
-  }
-  
-  if(keyDown("DOWN_ARROW")){
-    astronaut.addAnimation("gymming", gym);
-    astronaut.changeAnimation("gymming");
-    astronaut.y = 350;
-    astronaut.velocityX = 0;
-    astronaut.velocityY = 0;
-  }
-  
-  if(keyDown("LEFT_ARROW")){
-    astronaut.addAnimation("eating", eat);
-    astronaut.changeAnimation("eating");
-    astronaut.x = 150;
-    astronaut.y = 350;
-    astronaut.velocityX = 0.5;
-    astronaut.velocityY = 0.5;
-  }
-  
-  if(keyDown("RIGHT_ARROW")){
-    astronaut.addAnimation("bathing", bath);
-    astronaut.changeAnimation("bathing");
-    astronaut.x = 300;
-    astronaut.velocityX = 0;
-    astronaut.velocityY = 0;
-  }
-
-  if(keyDown("m")){
-    astronaut.addAnimation("moving", move);
-    astronaut.changeAnimation("moving");
-    astronaut.velocityX = 1;
-    astronaut.velocityY = 1;
-  }
-
-}
